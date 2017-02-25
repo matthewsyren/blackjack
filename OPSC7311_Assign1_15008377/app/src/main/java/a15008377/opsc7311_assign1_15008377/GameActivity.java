d2505a1f38f3974a023a3ce7655b1198033236c6
Matthew Syrén <matthew.syren@gmail.com>
2017-02-25 14:24:20 +0200

    Added functionality to display the dealer's hand and to determine the game's winner

--- OPSC7311_Assign1_15008377/app/src/main/java/a15008377/opsc7311_assign1_15008377/GameActivity.java
+++ OPSC7311_Assign1_15008377/app/src/main/java/a15008377/opsc7311_assign1_15008377/GameActivity.java
@@ -1,10 +1,14 @@
 package a15008377.opsc7311_assign1_15008377;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.GridView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -20,31 +24,84 @@ public class GameActivity extends AppCompatActivity {
     ArrayList<Integer> deckOfCards;
     ArrayList<Integer> userHand;
     ArrayList<Integer> dealerHand;
+    int userScore = 0;
+    int dealerScore = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game);
+        try{
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_game);
 
-        userHand = new ArrayList<>();
-        dealerHand = new ArrayList<>();
-        populateDeckOfCards();
+            userHand = new ArrayList<>();
+            dealerHand = new ArrayList<>();
+            populateDeckOfCards();
 
-        //Deals 2 cards each to the player and the dealer
-        for(int i = 0; i < 2; i++){
-            dealCard(userHand);
-            dealCard(dealerHand);
+            //Deals 2 cards each to the player and the dealer
+            for(int i = 0; i < 2; i++){
+                dealCard(userHand);
+                dealCard(dealerHand);
+            }
+
+            //Calculates the user and dealers' scores
+            userScore = calculateScore(userHand);
+            dealerScore = calculateScore(dealerHand);
+
+            //Displays the user's hand
+            displayUserHand();
+            displayHiddenDealerHand();
         }
-
-        //Displays the user's hand
-        displayUserHand();
+        catch(Exception exc){
+            Toast.makeText(getApplicationContext(), exc.getMessage(), Toast.LENGTH_LONG).show();
+        }
     }
 
     //Method displays the user's hand
     public void displayUserHand(){
-        final CardAdapter adapter = new CardAdapter(this, userHand);
-        GridView gridView = (GridView) findViewById(R.id.hand_of_cards);
-        gridView.setAdapter(adapter);
+        try{
+            displayHand(userHand, R.id.hand_of_cards);
+
+            //Displays the user's score
+            userScore = calculateScore(userHand);
+            TextView txtUserScore = (TextView) findViewById(R.id.label_user_score);
+
+            if (userScore > 21){
+                txtUserScore.setText("Your score is " + userScore + " points. You have gone bust...");
+                disableButtons();
+                determineWinner();
+            }
+            else{
+                txtUserScore.setText("Your score is " + userScore + " points...");
+            }
+        }
+        catch(Exception exc){
+            Toast.makeText(getApplicationContext(), exc.getMessage(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    //Method displays the dealer's hand
+    public void displayHiddenDealerHand(){
+        try{
+            ArrayList<Integer> dealerHiddenHand = new ArrayList<>();
+            dealerHiddenHand.add(dealerHand.get(0));
+            dealerHiddenHand.add(R.drawable.back_of_card);
+
+            displayHand(dealerHiddenHand, R.id.dealer_hand_of_cards);
+        }
+        catch(Exception exc){
+            Toast.makeText(getApplicationContext(), exc.getMessage(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void displayHand(ArrayList<Integer> lstCards, int view){
+        try{
+            final CardAdapter adapter = new CardAdapter(this, lstCards);
+            GridView gridView = (GridView) findViewById(view);
+            gridView.setAdapter(adapter);
+        }
+        catch(Exception exc){
+            Toast.makeText(getApplicationContext(), exc.getMessage(), Toast.LENGTH_LONG).show();
+        }
     }
 
     //Method adds all cards to the deckOfCards ArrayList
@@ -104,9 +161,6 @@ public class GameActivity extends AppCompatActivity {
             deckOfCards.add(R.drawable.ace_of_hearts);
             deckOfCards.add(R.drawable.ace_of_spades);
         }
-        catch(NullPointerException nfp){
-            Toast.makeText(getApplicationContext(), nfp.getMessage(), Toast.LENGTH_LONG).show();
-        }
         catch(Exception exc){
             Toast.makeText(getApplicationContext(), exc.getMessage(), Toast.LENGTH_LONG).show();
         }
@@ -119,9 +173,6 @@ public class GameActivity extends AppCompatActivity {
             lstCards.add(deckOfCards.get(cardNumber));
             deckOfCards.remove(cardNumber);
         }
-        catch(NullPointerException nfp){
-            Toast.makeText(getApplicationContext(), nfp.getMessage(), Toast.LENGTH_LONG).show();
-        }
         catch(Exception exc){
             Toast.makeText(getApplicationContext(), exc.getMessage(), Toast.LENGTH_LONG).show();
         }
@@ -133,8 +184,142 @@ public class GameActivity extends AppCompatActivity {
             dealCard(userHand);
             displayUserHand();
         }
-        catch(NullPointerException nfp){
-            Toast.makeText(getApplicationContext(), nfp.getMessage(), Toast.LENGTH_LONG).show();
+        catch(Exception exc){
+            Toast.makeText(getApplicationContext(), exc.getMessage(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    //Method calculates the scores for each user
+    public int calculateScore(ArrayList<Integer> lstCards) {
+        int score = 0;
+        int aceCount = 0;
+        try{
+            for (int i = 0; i < lstCards.size(); i++) {
+                int cardNumber = lstCards.get(i);
+
+                //Method assigns value to score based on the hexadecimal value of the images stored in the player's hand
+                if (cardNumber >= R.drawable.two_of_clubs && cardNumber <= R.drawable.two_of_spades) {
+                    score += 2;
+                } else if (cardNumber >= R.drawable.three_of_clubs && cardNumber <= R.drawable.three_of_spades) {
+                    score += 3;
+                } else if (cardNumber >= R.drawable.four_of_clubs && cardNumber <= R.drawable.four_of_spades) {
+                    score += 4;
+                } else if (cardNumber >= R.drawable.five_of_clubs && cardNumber <= R.drawable.five_of_spades) {
+                    score += 5;
+                } else if (cardNumber >= R.drawable.six_of_clubs && cardNumber <= R.drawable.six_of_spades) {
+                    score += 6;
+                } else if (cardNumber >= R.drawable.seven_of_clubs && cardNumber <= R.drawable.seven_of_spades) {
+                    score += 7;
+                } else if (cardNumber >= R.drawable.eight_of_clubs && cardNumber <= R.drawable.eight_of_spades) {
+                    score += 8;
+                } else if (cardNumber >= R.drawable.nine_of_clubs && cardNumber <= R.drawable.nine_of_spades) {
+                    score += 9;
+                } else if (cardNumber >= R.drawable.ace_of_clubs && cardNumber <= R.drawable.ace_of_spades) {
+                    score += 11;
+                    aceCount += 1;
+                } else {
+                    score += 10;
+                }
+            }
+
+            //Sets the appropriate number of aces to 1 if the user goes bust
+            if(score > 21 && aceCount > 0){
+                while(aceCount > 0 && score > 21){
+                    score -= 10;
+                    aceCount--;
+                }
+            }
+        }
+        catch(Exception exc){
+            Toast.makeText(getApplicationContext(), exc.getMessage(), Toast.LENGTH_LONG).show();
+        }
+        return score;
+    }
+
+    //Method deals the rest of the dealer's hand (once the player chooses to stay)
+    public void finishDealingDealerHand(View view){
+        try{
+            disableButtons();
+            displayHand(dealerHand, R.id.dealer_hand_of_cards);
+            dealerScore = calculateScore(dealerHand);
+            boolean dealAgain = dealerScore < 17;
+            while(dealAgain){
+                dealCard(dealerHand);
+                dealerScore = calculateScore(dealerHand);
+                dealAgain = dealerScore < 17;
+                displayHand(dealerHand, R.id.dealer_hand_of_cards);
+            }
+            determineWinner();
+        }
+        catch(Exception exc){
+            Toast.makeText(getApplicationContext(), exc.getMessage(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    //Method outputs the winner of the game, and offers the user the chance to play again
+    public void determineWinner(){
+        try{
+            AlertDialog alertDialog = new AlertDialog.Builder(GameActivity.this).create();
+
+            //Determines the winner based on the user and dealers' scores
+            if(userScore > 21){
+                alertDialog.setTitle("You lose!");
+                alertDialog.setMessage("You went bust, with a score of " + userScore + ".\n\nWould you like to play again?");
+            }
+            else if(dealerScore > 21){
+                alertDialog.setTitle("YOU WIN!");
+                alertDialog.setMessage("The dealer went bust, with a score of " + dealerScore + ". \n\nWould you like to play again?");
+            }
+            else{
+                if(dealerScore > userScore){
+                    alertDialog.setTitle("You lose!");
+                }
+                else if(userScore > dealerScore){
+                    alertDialog.setTitle("YOU WIN!");
+                }
+                else{
+                    alertDialog.setTitle("You draw!");
+
+                }
+                alertDialog.setMessage("The dealer scored " + dealerScore + ", while you scored " + userScore + ". \n\nWould you like to play again?");
+            }
+
+            //Creates OnClickListener for the Dialog message
+            DialogInterface.OnClickListener dialogOnClickListener = new DialogInterface.OnClickListener(){
+                @Override
+                public void onClick(DialogInterface dialog, int button) {
+                    Intent intent;
+                    switch(button){
+                        case AlertDialog.BUTTON_POSITIVE:
+                            intent = getIntent();
+                            finish();
+                            startActivity(intent);
+                            break;
+                        case AlertDialog.BUTTON_NEGATIVE:
+                            intent = new Intent(GameActivity.this, HomeActivity.class);
+                            startActivity(intent);
+                            break;
+                    }
+                }
+            };
+
+            //Assigns buttons and OnClickListener for the AlertDialog and displays the AlertDialog
+            alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "Yes", dialogOnClickListener);
+            alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, "No", dialogOnClickListener);
+            alertDialog.show();
+        }
+        catch(Exception exc){
+            Toast.makeText(getApplicationContext(), exc.getMessage(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    //Method disables the buttons on the screen to prevent the user from clicking them when they are not supposed to
+    public void disableButtons(){
+        try{
+            Button btnHit = (Button) findViewById(R.id.button_hit);
+            Button btnStay = (Button) findViewById(R.id.button_stay);
+            btnHit.setEnabled(false);
+            btnStay.setEnabled(false);
         }
         catch(Exception exc){
             Toast.makeText(getApplicationContext(), exc.getMessage(), Toast.LENGTH_LONG).show();
